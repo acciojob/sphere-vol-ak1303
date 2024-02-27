ff4468cb9a4973377c19b3f9ea3b88163c8e2863
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-28 00:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let radius = document.getElementById('radius').value;
+	if(radius<0)volume.value='NaN';
+	else{
+		let volume = document.getElementById('volume'); 
+		volume.value = ((4/3)*Math.PI*Math.pow(radius,3)).toFixed(4);	
+	}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
